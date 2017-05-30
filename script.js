994f6669fcb0f54fe07d070a7ad36a44ecaa4375
Charles Kim <ck1114@gmail.com>
2017-05-30 15:15:56 -0700

    hero/villain mode gameplay background assets added and displays properly according to selection

--- script.js
+++ script.js
@@ -2,12 +2,15 @@ $(document).ready(function(){
     $('.logo').delay(2000).animate({ left: '17%', top:'15%'}, 1000);
 });
 
+var game_mode;
+
 function hero_selected(){
     $('.ultron').addClass('grayscale').removeClass('dropshadow');
     $('.select_villain').removeClass('dropshadow');
     $('.select_hero').addClass('dropshadow');
     $('.captain_america').removeClass('grayscale').addClass('dropshadow');
-    $('.select_play').addClass('flash').bind('click', play_game)
+    $('.select_play').addClass('flash').bind('click', play_game);
+    game_mode = 'hero';
 }
 
 function villain_selected(){
@@ -15,10 +18,17 @@ function villain_selected(){
     $('.select_hero').removeClass('dropshadow');
     $('.select_villain').addClass('dropshadow');
     $('.ultron').removeClass("grayscale").addClass('dropshadow');
-    $('.select_play').addClass('flash').bind('click', play_game)
+    $('.select_play').addClass('flash').bind('click', play_game);
+    game_mode = 'villain';
 }
 
 function play_game(){
-    $('.main_page').addClass('animated fadeOut')
-
+    $('.loading_screen').addClass('animated fadeOut')
+    if (game_mode === 'hero'){
+        $('body').addClass('hero_background');
+    }
+    else if (game_mode === 'villain'){
+        $('body').addClass('villain_background');
+    }
 }
+
