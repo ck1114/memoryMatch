856c1e414067488b0fc3cb2d0016e87c44848444
Charles Kim <ck1114@gmail.com>
2017-05-29 18:27:59 -0700

    styling including animation completed for main page

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-/**
- * Created by ck111 on 5/27/2017.
- */
+$(document).ready(function(){
+    $('.logo').delay(2000).animate({ left: '17%', top:'15%'}, 1000);
+});
+
+function hero_selected(){
+    $('.ultron').addClass('grayscale').removeClass('dropshadow');
+    $('.select_villain').removeClass('dropshadow');
+    $('.select_hero').addClass('dropshadow');
+    $('.captain_america').removeClass('grayscale').addClass('dropshadow');
+    $('.select_play').addClass('flash');
+}
+
+function villain_selected(){
+    $('.captain_america').addClass('grayscale').removeClass('dropshadow');
+    $('.select_hero').removeClass('dropshadow');
+    $('.select_villain').addClass('dropshadow');
+    $('.ultron').removeClass("grayscale").addClass('dropshadow');
+    $('.select_play').addClass('flash');
+}
